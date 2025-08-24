1bc13916462b10c028d7c27b5a35edeb9dc6ebc5
nnv25 <nnv25@mail.ru>
2025-08-24 22:06:33 +0900

    240825

---
+++ frontend/src/components/JewerlyItem/JewerlyItem.jsx
@@ -0,0 +1,82 @@
+import { useContext, useRef } from "react";
+import "./JewerlyItem.css";
+import { assets } from "../../assets/assets_flowers";
+import { useParams, Link } from "react-router-dom";
+import { StoreContext } from "../Context/StoreContext";
+
+const JewerlyItem = ({ _id, name, price, description, images, shop }) => {
+  const { cartItems, addToCart, removeFromCart, url } =
+    useContext(StoreContext);
+  const sliderRef = useRef(null);
+
+  const handleNavigation = (index) => {
+    const sliderWidth = sliderRef.current.clientWidth;
+    sliderRef.current.scrollLeft = sliderWidth * index;
+  };
+  return (
+    <div className="jewerly-item">
+      <div className="jewerly-item-img-container">
+        <Link to={`/flowers/${_id}/${shop}`}>
+          <div className="slider" ref={sliderRef}>
+            {images.map((image, index) => (
+              <img
+                key={index}
+                className="jewerly-item-image"
+                src={`${url}/flower-images/${image}`}
+                alt={`flower-${index}`}
+              />
+            ))}
+          </div>
+        </Link>
+        <div className="slider-nav">
+          {images.map((_, index) => (
+            <span
+              key={index}
+              onClick={() => handleNavigation(index)}
+              className="slider-dot"
+            ></span>
+          ))}
+        </div>
+        {!cartItems[_id] ? (
+          <img
+            className="add"
+            onClick={() => {
+              addToCart(_id);
+            }}
+            src={assets.add_icon_white}
+            alt=""
+          />
+        ) : (
+          <div className="jewerly-item-counter">
+            <img
+              onClick={() => removeFromCart(_id)}
+              src={assets.remove_icon_red}
+              alt=""
+            />
+            <p>{cartItems[_id]}</p>
+            <img
+              onClick={() => {
+                addToCart(_id);
+              }}
+              src={assets.add_icon_green}
+              alt=""
+            />
+          </div>
+        )}
+      </div>
+      <Link to={`/flowers/${_id}/${shop}`}>
+        <div className="jewerly-item-info">
+          <div className="jewerly-item-name-rating">
+            <p>{name}</p>
+          </div>
+          <p className="jewerly-item-desc">{description}</p>
+          <p className="jewerly-item-price">
+            {Number(price).toLocaleString("ru-RU")} ₽
+          </p>
+        </div>
+      </Link>
+    </div>
+  );
+};
+
+export default JewerlyItem;
--- frontend/src/pages/Shop/Shop.jsx
+++ frontend/src/pages/Shop/Shop.jsx
@@ -1,13 +1,23 @@
-import { useContext, useEffect, useState } from 'react';
-import './Shop.css';
-import { StoreContext } from '../../components/Context/StoreContext';
-import { useParams, Link } from 'react-router-dom';
-import FlowerItem from '../../components/FlowerItem/FlowerItem';
-import PageSelector from '../../components/PageSelector/PageSelector'; // Импортируйте новый компонент
-import { assets } from '../../assets/assets_flowers';
+import { useContext, useEffect, useState } from "react";
+import "./Shop.css";
+import { StoreContext } from "../../components/Context/StoreContext";
+import { useParams, Link } from "react-router-dom";
+import JewerlyItem from "../../components/JewerlyItem/JewerlyItem";
+import PageSelector from "../../components/PageSelector/PageSelector"; // Импортируйте новый компонент
+import { assets } from "../../assets/assets_flowers";
 
 const Shop = () => {
-  const { url, limited_shop_list, limited_flowers_list, fetchFlowersListWithPagination, fetchFlowersList, flowerPagination, listRatings, fetchRatingsList, totalRatings, } = useContext(StoreContext);
+  const {
+    url,
+    limited_shop_list,
+    limited_flowers_list,
+    fetchFlowersListWithPagination,
+    fetchFlowersList,
+    flowerPagination,
+    listRatings,
+    fetchRatingsList,
+    totalRatings,
+  } = useContext(StoreContext);
   const { shopId } = useParams();
   const { currentPage = 1, totalPages = 1 } = flowerPagination || {};
   const limit = 1;
@@ -36,28 +46,38 @@ const Shop = () => {
 
   const formatDate = (dateString) => {
     const date = new Date(dateString);
-    const day = String(date.getDate()).padStart(2, '0');
-    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, "0");
+    const month = String(date.getMonth() + 1).padStart(2, "0");
     const year = date.getFullYear();
     return `${day}.${month}.${year}`;
   };
 
   return (
-    <div className='shop-container'>
+    <div className="shop-container">
       <div className="shop-info">
-        <p className='shop-name'>{shop.name}</p>
-        <p className="shop-rating"><img src={assets.daimond} alt="Рейтинг" className="star-logo-img" />{shop.averageRating}</p>
+        <p className="shop-name">{shop.name}</p>
+        <p className="shop-rating">
+          <img src={assets.daimond} alt="Рейтинг" className="star-logo-img" />
+          {shop.averageRating}
+        </p>
       </div>
-      <hr className='shop-info-divider' />
+      <hr className="shop-info-divider" />
       <div className="shop-display">
         <div className="shop-display-top">
           <div className="shop-display-logo">
-            <Link to={`/shop/${shopId}`}><img className="shop-logo" src={`${url}/images/${shop.image}`} alt={shop.name} /></Link>
+            <Link to={`/shop/${shopId}`}>
+              <img
+                className="shop-logo"
+                src={`${url}/images/${shop.image}`}
+                alt={shop.name}
+              />
+            </Link>
           </div>
-          <div className='shop-center-information'>
-            <div className='shop-work__information'>
-              <div className='shop-icon__time'>
-                <img className="time-icon" src={assets.time} alt="" /> <p className='work-time-desc'> Режим работы:</p>
+          <div className="shop-center-information">
+            <div className="shop-work__information">
+              <div className="shop-icon__time">
+                <img className="time-icon" src={assets.time} alt="" />{" "}
+                <p className="work-time-desc"> Режим работы:</p>
               </div>
               <div>
                 <p className="shop-information__desc">
@@ -75,25 +95,41 @@ const Shop = () => {
                 </p>
               </div>
             </div>
-            <div className='shop-payment__information'>
-              <div className='shop-icon__time'>
-                <img className="time-icon" src={assets.payment_icon} alt="" /> <p className='work-time-desc'> Форма оплаты:</p>
+            <div className="shop-payment__information">
+              <div className="shop-icon__time">
+                <img className="time-icon" src={assets.payment_icon} alt="" />{" "}
+                <p className="work-time-desc"> Форма оплаты:</p>
               </div>
-              <p className='shop-information'>{shop.payment_form}</p>
+              <p className="shop-information">{shop.payment_form}</p>
             </div>
           </div>
-          <div className='flower-display-shop'>
-            
-            <p className="shop-information">Адрес: {shop.address}</p>
-            
-            <p className='shop-information'>Доставка курьерской службой: {shop.delivery === true ? "Есть" : "Нет"}</p>
+          <div className="shop-left__information">
+            <div className="shop-icon__address">
+              <img className="time-icon" src={assets.address_icon} alt="" />{" "}
+              <p className="work-time-desc"> Адрес:</p>
+            </div>
+            <div className="shop-address__information">
+              <p className="shop-information__desc">{shop.address}</p>
+            </div>
+            <div className="shop-payment__information">
+              <div className="shop-icon__time">
+                <img className="time-icon" src={assets.delivery_icon} alt="" />{" "}
+                <p className="work-time-desc"> Доставка:</p>
+              </div>
+              <p className="shop-information">
+                {shop.delivery === true ? "Да" : "Нет"}
+              </p>
+            </div>
           </div>
         </div>
-        <hr className='shop-info-divider' />
-        <div className="flower-display-list">
+        <div className="h3-item">
+          <h3 className="item-h3">ТОВАРЫ</h3>
+        </div>
+        <hr className="shop-info-divider" />
+        <div className="shop-display-list">
           {limited_flowers_list.length > 0 ? (
             limited_flowers_list.map((flower) => (
-              <FlowerItem key={flower._id} {...flower} />
+              <JewerlyItem key={flower._id} {...flower} />
             ))
           ) : (
             <p>Нет доступных цветов в этом магазине.</p>
@@ -105,36 +141,51 @@ const Shop = () => {
         totalPages={totalPages}
         onPageChange={handlePageChange}
       />
-      <div className='rating-window'>
+      <div className="rating-window">
         <div>
-          <p className="shop-raiting"><img src={assets.Star} alt="Рейтинг" className="star-logo-img" />{shop.averageRating} ({totalRatings})</p>
+          <p className="shop-raiting">
+            <img src={assets.Star} alt="Рейтинг" className="star-logo-img" />
+            {shop.averageRating} ({totalRatings})
+          </p>
         </div>
-        <h3 className='raiting-h3'>Отзывы о магазине: "{shop.name}"</h3>
+        <h3 className="raiting-h3">Отзывы о магазине: "{shop.name}"</h3>
         {listRatings.map((item, index) => (
           <div key={index} className="rating rating-item-grid ">
-            <div className=' '>
-              <div className='rating-name flex'>
+            <div className=" ">
+              <div className="rating-name flex">
                 <p className="description">{item.userId.name}</p>
-                <p className='rating-user-comment'>{formatDate(item.createdAt)} г.</p>
+                <p className="rating-user-comment">
+                  {formatDate(item.createdAt)} г.
+                </p>
               </div>
-              <div className='flex'>
-                <p className="rating-number"><img src={assets.Star} alt="Рейтинг" className="rating-star" /></p>
+              <div className="flex">
+                <p className="rating-number">
+                  <img
+                    src={assets.Star}
+                    alt="Рейтинг"
+                    className="rating-star"
+                  />
+                </p>
                 <p>{item.rating}</p>
-                <p className='rating-user-comment'>{item.comment}</p>
+                <p className="rating-user-comment">{item.comment}</p>
               </div>
             </div>
-            <div className=''>
-              <div className='rating-name rating-shop-answer'>
+            <div className="">
+              <div className="rating-name rating-shop-answer">
                 <p>Ответ магазина:</p>
               </div>
 
-              <div className='rating-shop-answer-content'>
+              <div className="rating-shop-answer-content">
                 <p>{item.storeComment}</p>
               </div>
             </div>
           </div>
         ))}
-        <Link to={`/shop/${shopId}/rating/`}><button className="shop-item-button rating-button-shop ">Все отзывы</button></Link>
+        <Link to={`/shop/${shopId}/rating/`}>
+          <button className="shop-item-button rating-button-shop ">
+            Все отзывы
+          </button>
+        </Link>
       </div>
     </div>
   );
